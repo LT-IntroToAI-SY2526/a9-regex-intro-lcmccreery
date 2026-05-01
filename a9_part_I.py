28fed9e1344110c9c9b4841821c94822f34ee670
Liam Mccreery <lcmccreery@cps.edu>
2026-05-01 10:36:39 -0500

    commit change

--- a9_part_I.py
+++ a9_part_I.py
@@ -9,20 +9,20 @@ import re
 # should extract a match where the first group is the month, the second group the day
 # and the third group the year
 date_string = "November 9, 1982"
-pat = re.compile("REPLACE ME", re.IGNORECASE)
+pat = re.compile(r"([A-Za-z]+)\s+(\d{1,2}),\s+(\d{4})", re.IGNORECASE)
 date_matches = pat.match(date_string)
 
 # problem 2
 # should extract a match where the first group is the number, the second the street, the
 # third the city, the fourth the state and the fifth the zip code
 address_string = "2501 Addison Street\nChicago, IL 60618"
-pat = re.compile("REPLACE ME", re.IGNORECASE)
+pat = re.compile(r"(?P<number>\d+)\s+(?P<street>[A-Za-z ]+)\n(?P<city>[A-Za-z]+),\s+(?P<state>[A-Z]{2})\s+(?P<zip>\d{5}) ", re.IGNORECASE)
 address_matches = pat.match(address_string)
 
 # problem 3
 # should match all hashtags
 tweet_string = "hi everyone! #cs #python #LT #champions"
-pat = re.compile("REPLACE ME", re.IGNORECASE)
+pat = re.compile(r"#([A-Za-z0-9_]+)", re.IGNORECASE)
 hashtag_matches = pat.findall(tweet_string)
 
 # until you uncomment any code line below you'll get an EOF linting error feel free to
@@ -30,13 +30,13 @@ hashtag_matches = pat.findall(tweet_string)
 if __name__ == "__main__":
     print("<<<<< Date Problem >>>>>\n")
     # uncomment the following prints to see date results and asserts to test
-    # print(f"month is: {date_matches.group(1)}!") # should print "month is: November"
-    # print(f"day is: {date_matches.group(2)}!")   # should print "day is: 9"
-    # print(f"year is: {date_matches.group(3)}!")  # should print "year is: 1982"
-    # assert date_matches.group(1) == 'November', "Incorrect month"
-    # assert date_matches.group(2) == '9', "Incorrect day"
-    # assert date_matches.group(3) == '1982', "Incorrect year"
-    # print('\n<<<< Date extraction tests passed >>>>\n')
+    print(f"month is: {date_matches.group(1)}!") # should print "month is: November"
+    print(f"day is: {date_matches.group(2)}!")   # should print "day is: 9"
+    print(f"year is: {date_matches.group(3)}!")  # should print "year is: 1982"
+    assert date_matches.group(1) == 'November', "Incorrect month"
+    assert date_matches.group(2) == '9', "Incorrect day"
+    assert date_matches.group(3) == '1982', "Incorrect year"
+    print('\n<<<< Date extraction tests passed >>>>\n')
 
     print("<<<<< Address Problem >>>>>\n")
     # uncomment the following prints to see results and asserts to test
@@ -45,12 +45,12 @@ if __name__ == "__main__":
     # print(f'city is: {address_matches.group("city")}!')     # should print "city is: Chicago"
     # print(f'state is: {address_matches.group("state")}!')   # should print "state is: IL"
     # print(f'zip is: {address_matches.group("zip")}!')       # should print "zip is: 60618"
-    # assert address_matches.group('number') == '2501', "Incorrect address number"
-    # assert address_matches.group('street') == 'Addison Street', "Incorrect street"
-    # assert address_matches.group('city') == 'Chicago', "Incorrect city"
-    # assert address_matches.group('state') == 'IL', "Incorrect state"
-    # assert address_matches.group('zip') == '60618', "Incorrect zip"
-    # print('\n<<<< Address extraction tests passed >>>>\n')
+    assert address_matches.group('number') == '2501', "Incorrect address number"
+    assert address_matches.group('street') == 'Addison Street', "Incorrect street"
+    assert address_matches.group('city') == 'Chicago', "Incorrect city"
+    assert address_matches.group('state') == 'IL', "Incorrect state"
+    assert address_matches.group('zip') == '60618', "Incorrect zip"
+    print('\n<<<< Address extraction tests passed >>>>\n')
 
     print("<<<<< Hashtag Problem >>>>>\n")
     # uncomment the following prints to see results and asserts to test
--- a9_part_II.py
+++ a9_part_II.py
@@ -19,8 +19,8 @@ def get_planet_radius(planet_name: str) -> str:
         radius of the given planet
     """
     infobox_text = clean_text(get_first_infobox_text(get_page_html(planet_name)))
-    # TODO: fill this in
-    pattern = "REPLACE ME"
+    print(infobox_text)
+    pattern = "Polar radius(?P<radius>[\\d,]+)"
     error_text = "Page infobox has no polar radius information"
     match = get_match(infobox_text, pattern, error_text)
     return match.group("radius")
@@ -37,7 +37,7 @@ def get_birth_date(name: str) -> str:
     """
     infobox_text = clean_text(get_first_infobox_text(get_page_html(name)))
     # TODO: fill this in
-    pattern = "REPLACE ME"
+    pattern = r"(?P<birth>\d{4}-\d{2}-\d{2})"
     error_text = (
         "Page infobox has no birth information (at least none in xxxx-xx-xx format)"
     )
@@ -57,12 +57,12 @@ if __name__ == "__main__":
     print(f'Saturn has a polar radius of {get_planet_radius("Saturn")}km')
 
     # uncomment below lines for tests once you think you're getting the right output
-    # print('\n<<<< Running asserts, this might take a sec >>>>')
-    # assert get_planet_radius("Mars") == "3376.2", "Incorrect radius for Mars"
-    # assert get_planet_radius("Earth") == "6356.752", "Incorrect radius for Earth"
-    # assert get_planet_radius("Jupiter") == "66854", "Incorrect radius for Jupiter"
-    # assert get_planet_radius("Saturn") == "54364", "Incorrect radius for Saturn"
-    # print('\n<<<< Planet radius tests passed >>>>')
+    print('\n<<<< Running asserts, this might take a sec >>>>')
+    assert get_planet_radius("Mars") == "3376.2", "Incorrect radius for Mars"
+    assert get_planet_radius("Earth") == "6356.752", "Incorrect radius for Earth"
+    assert get_planet_radius("Jupiter") == "66854", "Incorrect radius for Jupiter"
+    assert get_planet_radius("Saturn") == "54364", "Incorrect radius for Saturn"
+    print('\n<<<< Planet radius tests passed >>>>')
 
     print("\n<<<<<<<<<<<<<< Testing Birth Dates >>>>>>>>>>>>>>")
     # should be 1906-12-09
@@ -75,11 +75,11 @@ if __name__ == "__main__":
     print(format_birth(get_birth_date("Anita Borg"), "Anita Borg"))
 
     # uncomment below lines for tests once you think you're getting the right output
-    # print('\n<<<< Running asserts, this might take a sec >>>>')
-    # assert get_birth_date("Grace Hopper") == "1906-12-09", "Incorrect birth date for Grace Hopper"
-    # assert get_birth_date("Alan Turing") == "1912-06-23", "Incorrect birth date for Alan Turing"
-    # assert get_birth_date("Tim Berners-Lee") == "1955-06-08", "Incorrect birth date for Tim Berners-Lee"
-    # assert get_birth_date("Anita Borg") == "1949-01-17", "Incorrect birth date for Anita Borg"
-    # print('\n<<<< Birth date tests passed >>>>')
+    print('\n<<<< Running asserts, this might take a sec >>>>')
+    assert get_birth_date("Grace Hopper") == "1906-12-09", "Incorrect birth date for Grace Hopper"
+    assert get_birth_date("Alan Turing") == "1912-06-23", "Incorrect birth date for Alan Turing"
+    assert get_birth_date("Tim Berners-Lee") == "1955-06-08", "Incorrect birth date for Tim Berners-Lee"
+    assert get_birth_date("Anita Borg") == "1949-01-17", "Incorrect birth date for Anita Borg"
+    print('\n<<<< Birth date tests passed >>>>')
 
-    # print('\n<<<< All tests passed! >>>>')
+    print('\n<<<< All tests passed! >>>>')
